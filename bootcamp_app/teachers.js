5146649079fe5ac8e6dd74cebe1627d4554ff712
EbukaMoneme <camoneme@gmail.com>
2021-10-14 05:02:18 +0000

    Connect javascript app to postgresql database using node-postgres library

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const input = process.argv[2];
+
+const sqlQuery = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${input || 'JUL02'}'
+ORDER BY teacher;
+`;
+
+pool.query(sqlQuery)
+.then(res => {
+  res.rows.forEach(request => {
+    console.log(`${request.cohort}: ${request.teacher}`)
+  })
+})
+.catch(err => console.error('query error', err.stack))
